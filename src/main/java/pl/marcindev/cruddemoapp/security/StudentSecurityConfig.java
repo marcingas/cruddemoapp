4a3c7d2dbcf3ccb2673ced874f2ef89de74b724b
Marcin <gasiorek.mail@gmail.com>
2023-06-28 13:40:41 +0200

    added access security for users based on URL

--- src/main/java/pl/marcindev/cruddemoapp/security/StudentSecurityConfig.java
+++ src/main/java/pl/marcindev/cruddemoapp/security/StudentSecurityConfig.java
@@ -2,9 +2,13 @@ package pl.marcindev.cruddemoapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class StudentSecurityConfig {
@@ -26,6 +30,20 @@ public class StudentSecurityConfig {
                 .roles("EMPLOYEE","MANAGER","ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(john,mary,ann);
+    }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
+http.authorizeHttpRequests(configurer->
+        configurer
+        .requestMatchers(HttpMethod.GET,"/main/students").hasRole("EMPLOYEE")
+        .requestMatchers(HttpMethod.GET,"/main/students/**").hasRole("EMPLOYEE")
+        .requestMatchers(HttpMethod.POST,"/main/students").hasRole("MANAGER")
+        .requestMatchers(HttpMethod.PUT,"/main/students").hasRole("MANAGER")
+        .requestMatchers(HttpMethod.DELETE,"/main/students").hasRole("ADMIN")
+
+);
+http.httpBasic(Customizer.withDefaults());
+  return http.build();
     }
 }
