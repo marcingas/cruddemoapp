847ffa1acf45363af6202a1f3829130c80769386
Marcin <gasiorek.mail@gmail.com>
2023-06-28 14:14:15 +0200

    added Autenthication using JDBC

--- src/main/java/pl/marcindev/cruddemoapp/security/StudentSecurityConfig.java
+++ src/main/java/pl/marcindev/cruddemoapp/security/StudentSecurityConfig.java
@@ -8,29 +8,39 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class StudentSecurityConfig {
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}123")
-                .roles("EMPLOYEE")
-                .build();
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}123")
-                .roles("EMPLOYEE","MANAGER")
-                .build();
-        UserDetails ann = User.builder()
-                .username("ann")
-                .password("{noop}123")
-                .roles("EMPLOYEE","MANAGER","ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(john,mary,ann);
+    UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
+
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//        UserDetails john = User.builder()
+//                .username("john")
+//                .password("{noop}123")
+//                .roles("EMPLOYEE")
+//                .build();
+//        UserDetails mary = User.builder()
+//                .username("mary")
+//                .password("{noop}123")
+//                .roles("EMPLOYEE","MANAGER")
+//                .build();
+//        UserDetails ann = User.builder()
+//                .username("ann")
+//                .password("{noop}123")
+//                .roles("EMPLOYEE","MANAGER","ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(john,mary,ann);
+//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
