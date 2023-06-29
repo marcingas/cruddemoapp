f38b678f29e704fe01b1cbffbfbcd9ba19fab340
Marcin <gasiorek.mail@gmail.com>
2023-06-29 11:06:41 +0200

    use bcryot password for security

--- src/main/resources/scriptSecurity.sql
+++ src/main/resources/scriptSecurity.sql
@@ -1,16 +1,19 @@
 USE student_tracker;
+DROP TABLE IF EXISTS `authorities`;
+DROP TABLE IF EXISTS `users`;
+
 CREATE TABLE `users` (
 `username` varchar(50) NOT NULL,
-`password` varchar(50) NOT NULL,
+`password` varchar(68) NOT NULL,
 `enabled` tinyint NOT NULL,
 PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `users`
 VALUES
-('john', '{noop}123',1),
-('mary','{noop}123',1),
-('ann','{noop}123',1);
+('john', '{bcrypt}$2a$10$eFn0NixyHKA9eMRY5QEZr.JhcCl.Mld0GlxX0KEW.emm4h14.OKL2',1),
+('mary','{bcrypt}$2a$10$eFn0NixyHKA9eMRY5QEZr.JhcCl.Mld0GlxX0KEW.emm4h14.OKL2',1),
+('ann','{bcrypt}$2a$10$eFn0NixyHKA9eMRY5QEZr.JhcCl.Mld0GlxX0KEW.emm4h14.OKL2',1);
 
 CREATE TABLE `authorities` (
 `username` varchar(50) NOT NULL,
